34784173fbfd4ee3c0d4cb24a3abdaec326d4a5f
Diana Gehring <dianagehring@dianas-air.gateway.2wire.net>
2013-11-20 23:58:33 -0800

    added a test class

---
+++ DGTest.h
@@ -0,0 +1,13 @@
+//
+//  DGTest.h
+//  GitTest
+//
+//  Created by Diana Gehring on 11/20/13.
+//  Copyright (c) 2013 Diana Gehring. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DGTest : NSObject
+
+@end
---
+++ DGTest.m
@@ -0,0 +1,13 @@
+//
+//  DGTest.m
+//  GitTest
+//
+//  Created by Diana Gehring on 11/20/13.
+//  Copyright (c) 2013 Diana Gehring. All rights reserved.
+//
+
+#import "DGTest.h"
+
+@implementation DGTest
+
+@end
